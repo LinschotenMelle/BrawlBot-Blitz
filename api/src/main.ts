46c1fdd2f46d39306aaf8ca2fbeddd48e95cc132
Melle Linschoten <m.linschoten@schippers.eu>
2024-04-15 08:37:11 +0200

    Added api

---
+++ api/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as session from 'express-session';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+    store: new session.MemoryStore(),
+  }))
+  try {
+    await app.listen(process.env.PORT || 3001);
+    console.log(`Server is running on: ${await app.getUrl()}`);
+  } catch (e) {
+    console.error(e);
+  }
+}
+bootstrap();
